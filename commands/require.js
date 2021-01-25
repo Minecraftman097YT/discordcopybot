49b2dd97dc33ece651a6bedf15969ce907e9c681
Glitch (hello-webpage) <none>
2021-01-25 17:56:59 +0000

    💽🍧 Checkpoint ./commands/require.js:6616179/837

---
+++ commands/require.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+exports.run = (bot, message, args) => {
+
+
+    const Help = new Discord.RichEmbed()
+      .setTitle("**Requirments**")
+      .addField("Level:", "***50+***")
+      .addField("Average:", "***800+***")
+      .addField("Survival Time:", "***40 Minutes or More***")
+      .addField("Highscore:", "***40K+***")
+      .setColor("RANDOM")
+    message.channel.send(Help);
+    message.delete()
+}
