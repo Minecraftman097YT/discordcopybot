b2ea9dc993cb50ec479e1247b95b6178f763c763
Glitch (alluring-pine-zydeco) <none>
2021-02-05 12:19:12 +0000

    🗾🦁 Checkpoint ./commands/help.js:6616179/2 ./commands/ping.js:6616179/64

--- commands/ping.js
+++ commands/ping.js
@@ -3,15 +3,11 @@ const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
   
   let embed = new Discord.RichEmbed()
-  .setTitle("Pong! :ping_pong:")
-   .setThumbnail(bot.user.avatarURL) .setThumbnail(bot.user.avatarURL) .setDescription(`${bot.ping} ms`) 
-     
-  
-  .setTimestamp()
-
- 
-  
-  .setColor("DARKBLUE")
+   .setTitle("Pong! :ping_pong:")
+   .setThumbnail(bot.user.avatarURL) 
+   .setDescription(`${bot.ping} ms`) 
+   .setTimestamp()
+   .setColor("DARKBLUE")
   message.channel.send(embed)
   message.delete()
 };
