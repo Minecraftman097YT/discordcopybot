73f66230b731a23700d70a887cc10157dbd637fa
Glitch (tkrbotiscord) <none>
2021-04-04 22:25:24 +0000

    🐿🎰 Checkpoint ./commands/help.js:6616179/1874 ./index.js:6616179/4672 ./config.json:6616179/93 ./storage/config.json:6616179/19

--- commands/help.js
+++ commands/help.js
@@ -1,25 +1,24 @@
 const Discord = require("discord.js");
 
 exports.run = (bot, message, args) => {
-
-const exampleEmbed = new Discord.MessageEmbed()
-      .setTitle("**GamingTreff Commands:**", "------------------------")
-      .addField("**_partner**","***Sehe unseren Partnertext.***")
-      .addField("**_meme**", "***Schau dir Lustige Memes an ;)***")
-      .addField("**_ping**", "***Schau dir gerne den Bot-Ping an.***")
-      .addField("**_rules**", "***Schau dir die Regeln an.***")
-      .addField("**_botinfo**", "***Schau dir die Botinfo an.***")
-      .addField("**_serverinfo**", "***Schau dir Serverinfo an.***")
-      .addField("**_report**", "***Melde ein Mitglied.***")
-      .setColor("GREEN")
-      .setFooter(`Viel Spaß`);
-    channel.send(exampleEmbed);
-    message.delete()
-}
+  const exampleEmbed = new Discord.MessageEmbed()
+    .setTitle("**GamingTreff Commands:**", "------------------------")
+    .addField("**_partner**", "***Sehe unseren Partnertext.***")
+    .addField("**_meme**", "***Schau dir Lustige Memes an ;)***")
+    .addField("**_ping**", "***Schau dir gerne den Bot-Ping an.***")
+    .addField("**_rules**", "***Schau dir die Regeln an.***")
+    .addField("**_botinfo**", "***Schau dir die Botinfo an.***")
+    .addField("**_serverinfo**", "***Schau dir Serverinfo an.***")
+    .addField("**_report**", "***Melde ein Mitglied.***")
+    .setColor("GREEN")
+    .setFooter(`Viel Spaß`);
+  channel.send(exampleEmbed);
+  message.delete();
+};
 module.exports.command = {
-    name: "help",
-    aliases: ["ui"],
-    description: "Gives user information of tagged user of you!",
-    category: "Util",
-    usage: "help"
-}
+  name: "help",
+  aliases: ["ui"],
+  description: "Gives user information of tagged user of you!",
+  category: "Util",
+  usage: "help"
+};
