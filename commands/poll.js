2df0e793f6bb2cb5dd36bb1de91e8a0ae2badaee
Glitch (tkrbotiscord) <none>
2021-04-14 19:20:02 +0000

    🍝🍞 Checkpoint ./commands/poll.js:6616179/1613

--- commands/poll.js
+++ commands/poll.js
@@ -1,18 +1,33 @@
 const Discord = require("discord.js");
-
-exports.run = (bot, message, args) => {
-  let msg = args.slice(0).join(" ")
-  let user1 = message.mentions.users.first();
-
-  if (!msg) return message.reply("Bitte schreibe noch ein Satz dahinter.")
-
-  let embedPoll = new Discord.RichEmbed()
-  .setTitle("Neue Umfrage")
-  .setDescription(`${msg}`)
-  .setColor("BLUE")
-  .setTimestamp()
-  message.delete()
-  let msgEmbed = await pollChannel.send(embedPoll);
-  await msgEmbed.react("✅")
-  await msgEmbed.react("❌")
+module.exports = {
+  name: "poll",
+  description: "Create a simple yes or no poll",
+  category: "fun",
+  run: async (bot, message, args) => {
+    if (!message.member.permissions.has("ADMINISTRATOR"))
+      return message.channel.send(
+        `You do not have admin, ${message.author.username}`
+      );
+    const channel =
+      message.mentions.channels.first() ||
+      message.guild.channels.cache.get(args[0]);
+    if (!channel) {
+      return message.channel.send(
+        `You did not mention / give the id of your channel!`
+      );
+    }
+    let question = message.content
+      .split(`${bot.prefix}poll ${channel} `)
+      .join("");
+    if (!question)
+      return message.channel.send(`You did not specify your question!`);
+    const Embed = new Discord.MessageEmbed()
+      .setTitle(`New poll!`)
+      .setDescription(`${question}`)
+      .setFooter(`${message.author.username} created this poll.`)
+      .setColor(`RANDOM`);
+    let msg = await bot.channels.cache.get(channel.id).send(Embed);
+    await msg.react("👍");
+    await msg.react("👎");
+  },
 };
