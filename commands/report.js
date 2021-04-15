61dfa51b4d2ed365a85f9e75f66de34234485506
Glitch (tkrbotiscord) <none>
2021-04-15 11:06:32 +0000

    Rewound to commit 8121399c90d8e5f694c32d10be547cd62daef9a5 + 0 edits

--- commands/report.js
+++ commands/report.js
@@ -1,51 +1,37 @@
-const { MessageEmbed } = require("discord.js");
-module.exports = {
-  name: "report",
-  category: "moderation",
-  description: "Report a user of your choice!",
-  usage: "<User mention>",
-  run: async (bot, message, args) => {
-    if (!message.member.permissions.has("MANAGE_MESSAGES"))
-      return message.channel.send(`Nope ;)`);
-    let User = message.mentions.users.first() || null;
+const Discord = require("discord.js");
+const moment = require("moment");
+require("moment-duration-format");
 
-    if (User == null) {
-      return message.channel.send(`Du Hast kein User Getagt`);
-    } else {
-      let Reason = message.content.slice(bot.prefix.length + 22 + 7) || null;
-      if (Reason == null) {
-        return message.channel.send(
-          `Du hast keinen Grund Angegeben!`
-        );
-      }
-      let Avatar = User.displayAvatarURL();
-      let Channel = message.guild.channels.cache.find(
-        (ch) => ch.name === "reports"
-      );
-      if (!Channel)
-        return message.channel.send(
-          `Es gibt auf diesen Server keinen Channel Namens #reports`
-        );
-      let Embed = new MessageEmbed()
-        .setTitle(`Neuer report!`)
-        .setDescription(
-          `Der User \`${message.author.tag}\` Hat \`${User.tag}\` Gemeldet! `
-        )
-        .setColor(`RED`)
-        .setThumbnail(Avatar)
-        .addFields(
-          { name: "Mod ID", value: `${message.author.id}`, inline: true },
-          { name: "Mod Tag", value: `${message.author.tag}`, inline: true },
-          { name: "Reported ID", value: `${User.id}`, inline: true },
-          { name: "Reported Tag", value: `${User.tag}`, inline: true },
-          { name: "Reason", value: `\`${Reason.slice(1)}\``, inline: true },
-          {
-            name: "Date (M/D/Y)",
-            value: `${new Intl.DateTimeFormat("en-US").format(Date.now())}`,
-            inline: true,
-          }
-        );
-      Channel.send(Embed);
-    }
-  },
+exports.run = (bot, message, args) => {
+  let member = message.mentions.members.first() || message.member;
+  let member1 = message.mentions.users.first() || message.author;
+  let grund2 = args.slice(1).join(" ");
+
+  if (!grund2) {
+    let info = new Discord.RichEmbed()
+      .setTitle("Bitte Benutze: _report {user} {Grund}")
+      .setColor("RED")
+      .setTimestamp();
+    message.channel.send(info);
+    message.delete();
+  } else {
+    let info = new Discord.RichEmbed()
+      .setTitle("REPORT")
+      .addField("User", member, true)
+      .addField("Grund", grund2)
+      .addField(
+        "Datum",
+        `${moment
+          .utc("Europe/Berlin")
+          .locale("de")
+          .format("dddd, Do MMMM YYYY, HH:mm:ss")}`,
+        true
+      )
+      .addField("__Bestätigung:__", "Das Mitglied wurde Erfolgreich Gemeldet.")
+      .setColor("GREEN")
+      .setTimestamp()
+      .setThumbnail(member1.avatarURL);
+    message.channel.send(info);
+    message.delete();
+  }
 };
