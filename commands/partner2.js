90200ce0625fdaff37b664f7cf9af7b52db6a323
Glitch (tkrbotiscord) <none>
2021-04-15 23:15:23 +0000

    🌅💶 Checkpoint ./commands/partner2.js:6616179/2038

--- commands/partner2.js
+++ commands/partner2.js
@@ -1,12 +1,16 @@
 const Discord = require("discord.js");
 
 exports.run = (bot, message, args) => {
-
-    const Help = new Discord.RichEmbed()
-      .setTitle("**Prime Empire**", "------------------------")
-      .addField("")
-      .setColor("RED")
-      .setFooter(`.`);
-    message.channel.send(Help);
-    message.delete()
-}    
+  const Help = new Discord.RichEmbed()
+    .setTitle(" **Prime Empire** ", "------------------------")
+    .setDescription(
+      "<a:info:827806069404205076> **Joint** gerne Prime Empire, denn wir bieten sehr viele **Coole** Sachen! **Wie zB:**"
+    )
+    .addField(":performing_arts:", "Self Roles")
+    .addField(":speech_balloon:", "Viele Chats")
+    .addField(":loud_sound:", "Viele Sprachkanäle")
+    .setColor("ORANGE")
+    .setFooter(``);
+  message.channel.send(Help);
+  message.delete();
+};
