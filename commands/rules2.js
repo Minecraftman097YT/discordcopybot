ce56593e44091ec34d32224f5c1aad3d3f4a43a1
Glitch (tkrbotiscord) <none>
2021-04-17 06:37:20 +0000

    🏵👟 Checkpoint ./commands/rules2.js:6616179/1124 ./commands/rules.js:6616179/18

--- commands/rules2.js
+++ commands/rules2.js
@@ -1,28 +1,12 @@
 const Discord = require("discord.js");
 
 exports.run = (bot, message, args) => {
-  const Help = new Discord.RichEmbed()
-    .setTitle("<:PrimeEmpire:820742150915358761> **Prime Empire Regelwerk** ", "------------------------")
-    .setDescription(
-      "<a:info:827806069404205076> Lest euch die Regeln Durch, um keine Verwarnung zu Bekommen."
-    )
-    .addField("", ".")
-    .addField("", ".")
-    .addField("", ".")
-    .addField("", ".")
-    .addField("", ".")
-    .addField("", ".")
-    .addField("", ".")
-    .addField("", ".")
-    .addField("", ".")
-    .addField("", ".")
-    .addField("", ".")
-    .addField("", ".")
-    .addField("", ".")
-    .addField("")
-    .addField("Invite:", "[Klicke Hier](https://discord.gg/qAjrrkQADQ)")
-    .setColor("GREEN")
-    .setFooter(``);
-  message.channel.send(Help);
-  message.delete();
-};
+
+    const Help = new Discord.RichEmbed()
+      .setTitle("**Prime Empire**", "------------------------")
+      .addField("**Wartung!**","***Der Befehl '_Rules' Befindet sich Aktuell in einer Wartungsphase! Bitte Warten.***")
+      .setColor("RED")
+      .setFooter(`.`);
+    message.channel.send(Help);
+    message.delete()
+}    
