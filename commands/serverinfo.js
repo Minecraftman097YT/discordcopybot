4966f155668923da7fcb115da9dc77146b82989e
Glitch (tkrbotiscord) <none>
2021-04-18 20:32:09 +0000

    🤗📌 Checkpoint ./commands/serverinfo.js:6616179/4743 ./index.js:6616179/304

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,32 +1,88 @@
-const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const moment = require('moment');
-require('moment-duration-format');
 
-exports.run = (bot, message, prefixes) => {
-  
-  const embed = new Discord.RichEmbed()
-  .setAuthor(message.guild.name, message.guild.iconURL)
-  .setColor(3447003)
-  .addField("Server ID" , `(${message.guild.id})`)  
-  .addField('Owner' , `${message.guild.owner.user}`) // uff bot 
-  .addField('Member Counter' , `${message.guild.memberCount}`, true)
-  .addField('Region' , message.guild.region, true)
-  .addField("Server Erstellt" , `${moment.utc(message.guild.createdAt).locale('de').format("dddd, Do. MMMM YYYY")}`, true)
-  .setTimestamp()
-  .setColor("GREEN")
-  message.channel.send({embed})
-  message.react('✅')
+const filterLevels = {
+    DISABLED: 'Off',
+    MEMBERS_WITHOUT_ROLES: 'No Role',
+    ALL_MEMBERS: 'Everyone'
 };
 
-exports.conf = {
-  enabled: true,
-  aliases: ['info','stats','serverstats','guildinfo','guildstats'],
-  permLevel: 0
+const verificationLevels = {
+    NONE: 'None',
+    LOW: 'Low',
+    MEDIUM: 'Medium',
+    HIGH: '(╯°□°）╯︵ ┻━┻',
+    VERY_HIGH: '┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻'
 };
 
-exports.help = {
-  name: 'serverinfo',
-  description: 'Displays server information & statistics',
-  usage: 'serverinfo',
-  category: 'General',
+const regions = {
+    brazil: 'Brazil',
+    europe: 'Europe',
+    hongkong: 'Hong Kong',
+    india: 'India',
+    japan: 'Japan',
+    russia: 'Russia',
+    singapore: 'Singapore',
+    southafrica: 'South Africa',
+    sydeny: 'Sydeny',
+    'us-central': 'US Central',
+    'us-east': 'US East',
+    'us-west': 'US West',
+    'us-south': 'US South'
 };
+
+module.exports = {
+    name: 'server',
+    aliases: [],
+    description: 'server info...',
+    cooldown: 5,
+    guildOnly: false,
+    args: false,
+ run: async (client, message, args) => {
+        const roles = message.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString());
+        const members = message.guild.members.cache;
+        const channels = message.guild.channels.cache;
+        const emojis = message.guild.emojis.cache;
+
+        const embed = new MessageEmbed()
+            .setDescription(`**Server Info**`)
+            .setColor('BLACK')
+            .setThumbnail(message.guild.iconURL({ dynamic: true }))
+            .addField('General', [
+                `**Name:** ${message.guild.name}`,
+                `**ID:** ${message.guild.id}`,
+                `**Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
+                `**Region:** ${regions[message.guild.region]}`,
+                `**Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
+                `**Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
+                `**Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
+                `**Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} [${moment(message.guild.createdTimestamp).fromNow()}]`,
+                '\u200b'
+            ])
+            .addField('Statistics', [
+                `**Role Count:** ${roles.length}`,
+                `**Emoji Count:** ${emojis.size}`,
+                `**Regular Emoji Count:** ${emojis.filter(emoji => !emoji.animated).size}`,
+                `**Animated Emoji Count:** ${emojis.filter(emoji => emoji.animated).size}`,
+                `**Member Count:** ${message.guild.memberCount}`,
+                `**Humans:** ${members.filter(member => !member.user.bot).size}`,
+                `**Bots:** ${members.filter(member => member.user.bot).size}`,
+                `**Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
+                `**Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
+                `**Boost Count:** ${message.guild.premiumSubscriptionCount || '0'}`,
+                '\u200b'
+            ])
+            .addField('Presence', [
+                `**Online:** ${members.filter(member => member.presence.status === 'online').size}`,
+                `**Idle:** ${members.filter(member => member.presence.status === 'idle').size}`,
+                `**Do Not Disturb:** ${members.filter(member => member.presence.status === 'dnd').size}`,
+                `**Offline:** ${members.filter(member => member.presence.status === 'offline').size}`,
+                '\u200b'
+            ])
+            .addField(`Roles [${roles.length - 1}]`, roles.join(', '))
+    
+            .setTimestamp();
+        message.channel.send(embed);
+    }
+
+}
--- index.js
+++ index.js
@@ -28,7 +28,7 @@ bot.on("message", message => {
 
     let commandFile = require(`./commands/${cmd}.js`);
 
-    commandFile.run(bot, message, args);
+    commandFile.execute(bot, message, args);
   } catch (e) {
     console.log(e.stack);
   }
