1c8161df7d5f8da1596039cdcc7f65a566d04ab5
Glitch (tkrbotiscord) <none>
2021-04-19 20:57:38 +0000

    💎🍎 Checkpoint ./commands/partner2.js:6616179/1987 ./commands/egw.js:6616179/1715

--- commands/partner2.js
+++ commands/partner2.js
@@ -7,9 +7,11 @@ exports.run = (bot, message, args) => {
       "<a:info:827806069404205076> **Joint** gerne Prime Empire, denn wir bieten sehr viele **Coole** Sachen! **Wie zB:**"
     )
     .addField(":performing_arts: Self Roles \n\n:speech_balloon: Viele Chats", "Und")
-    .addField(":loud_sound: Viele Sprachkanäle \n\n:heavy_plus_sign: Join4Create \n\n:game_die: Viele Minigames", "Und") 
-    .addField(":tada: Viele Giveaways \n\n<a:Alarm:827807392589676564> Guten Support \n\n:handshake: Partnerschaften ab 80 Usern. \n\n<a:Stern:827807184887873538> Aktive Community", "Und noch")
-    .addField(":joy: Viele Memes \n\n:bulb: Wir Hören auf die Community \n\n:no_entry_sign: Keine @everyone Pings \n\n:globe_with_meridians: Und Vieles mehr!", "<a:info:827806069404205076> Unser Ziel sind die **200 Member**, also kommt gerne drauf, du wirst es nicht bereuen!")
+    .addField(":loud_sound: Viele Sprachkanäle \n\n:heavy_plus_sign: Join4Create \n\n
+    .addField(":game_die: Viele Minigames \n\n:handshake: Partnerschaften ab 80 Usern.", "Und") 
+    .addField(":tada: Viele Giveaways \n\n<a:Alarm:827807392589676564> Guten Support 
+    .addField("\n\n<a:Stern:827807184887873538> Aktive Community \n\n", "Und noch")
+    .addField(":joy: Viele Memes \n\n:bulb: Wir Hören auf die Community \n\n:globe_with_meridians: Und Vieles mehr!", "<a:info:827806069404205076> Unser Ziel sind die **200 Member**, also kommt gerne drauf, du wirst es nicht bereuen!")
     .addField("Link:", "[Klicke Hier](https://discord.gg/qAjrrkQADQ)")
     .setColor("ORANGE")
     .setImage("https://cdn.discordapp.com/attachments/811928066833776671/832110695037927434/image0.gif")
