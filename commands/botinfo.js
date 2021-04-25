cc7d7bde9b3e2b80c91f9e9d8bf16e4464169c83
Glitch (tkrbotiscord) <none>
2021-04-25 07:33:26 +0000

    🚤🎍 Checkpoint ./commands/botinfo.js:6616179/103

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -3,15 +3,15 @@ const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
   const Help = new Discord.RichEmbed()
     .setTitle("**__Bot Info__**")
-    .addField("👑BotName👑:", "``Op-Pro Community``")
-    .addField("👷‍Erstellt am👷‍:", "``Montag, 01.06.2020 um 07:22 Uhr``")
+    .addField("👑BotName👑:", "``Prime Empire Bot``")
+    .addField("👷‍Erstellt am👷‍:", "``Unknown``")
     .addField(
       "👨‍💼BotOwner👨‍💼:",
       "`Minecraftman097#9999`"
     )
     .addField("🤖Botversion🤖:", "`12.0.0`")
     .addField("👽BotSprache👽:", "`Javascript`")
-    .addField("**User**", ` ${bot.users.size}`)
+    .addField("**User**", `Unknown`)
     .setColor("DARKBLUE")
     .setThumbnail(bot.user.avatarURL)
     .setFooter(`Angefragt von: ${message.author.tag}`);
