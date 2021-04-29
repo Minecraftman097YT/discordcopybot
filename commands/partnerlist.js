5c8e8abc38f9717188dbdf01b46592428d067641
Glitch (tkrbotiscord) <none>
2021-04-29 11:05:33 +0000

    👾🏚 Checkpoint ./commands/partnerlist.js:6616179/1164

--- commands/partnerlist.js
+++ commands/partnerlist.js
@@ -6,9 +6,11 @@ exports.run = (bot, message, args) => {
     .setDescription(
       "<a:info:827806069404205076> **Hier werden die Partner von** ***Prime Empire*** **Aufgelistet**!"
     )
-    .addField("Partner1 ", "[Link]")
+    .addField("TLF \nInhaber: TLF#0001", "[Klicke Hier](TLF
+#0001)\n**---------------**")
+    .addField("Prime Empire \nInhaber: Elmar805 ツ#0805", "[Klicke Hier](https://discord.gg/qAjrrkQADQ)\n**---------------**")
     .setColor("ORANGE")
-    .setFooter(``);
+    .setFooter(`Wenn du Möchtest, Kannst du Gerne auch einen Unserem Partner Server Beitreten. Das Beitreten eines Partner Servers ist nicht Pflicht!`);
   message.channel.send(Partner);
   message.delete();
 };
