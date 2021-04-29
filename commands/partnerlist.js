5352f85a49c30e35cbbedf9cb3a67293f7ac7beb
Glitch (tkrbotiscord) <none>
2021-04-29 11:16:37 +0000

    🎰🚒 Checkpoint ./commands/partnerlist.js:6616179/3971

--- commands/partnerlist.js
+++ commands/partnerlist.js
@@ -2,15 +2,26 @@ const Discord = require("discord.js");
 
 exports.run = (bot, message, args) => {
   const Partner = new Discord.RichEmbed()
-    .setTitle("<:PrimeEmpire:820742150915358761> **Prime Empire** ", "------------------------")
-    .setDescription(
-      "<a:info:827806069404205076> **Hier werden die Partner von** ***Prime Empire*** **Aufgelistet**!"
+    .setTitle(
+      "<:PrimeEmpire:820742150915358761> **Prime Empire** ",
+      "------------------------"
     )
-    .addField("TLF \nInhaber: TLF#0001", "[Klicke Hier](TLF
-#0001)\n**---------------**")
-    .addField("Prime Empire \nInhaber: Elmar805 ツ#0805", "[Klicke Hier](https://discord.gg/qAjrrkQADQ)\n**---------------**")
+    .setDescription(
+      "<a:info:827806069404205076> **Hier werden die Partner von** ***Prime Empire*** **Aufgelistet**!")
+    .addField("TLF \nInhaber: TLF#0001","[Klicke Hier](https://discord.gg/92k6PhNtYq)\n**---------------**")
+    .addField("Community \nInhaber: Miky#2546", "[Klicke Hier](https://discord.gg/QxczbuRJ87)\n**---------------**")
+    .addField("Kuschelecke \nInhaber: Einfach nur ich#1844", "[Klicke Hier](https://discord.com/invite/S6uYxhvUNb)\n**---------------**")
+    .addField("Team Toxic \nInhaber: ", "[Klicke Hier](https://discord.gg/KUfR7JNV)\n**---------------**")
+    .addField("Kuschelecke \nInhaber: ", "[Klicke Hier]()\n**---------------**")
+    .addField("Kuschelecke \nInhaber: ", "[Klicke Hier]()\n**---------------**")
+    .addField("Kuschelecke \nInhaber: ", "[Klicke Hier]()\n**---------------**")
+    .addField("Kuschelecke \nInhaber: ", "[Klicke Hier]()\n**---------------**")
+    .addField("Kuschelecke \nInhaber: ", "[Klicke Hier]()\n**---------------**")
+    .addField("Kuschelecke \nInhaber: ", "[Klicke Hier]()\n**---------------**")
     .setColor("ORANGE")
-    .setFooter(`Wenn du Möchtest, Kannst du Gerne auch einen Unserem Partner Server Beitreten. Das Beitreten eines Partner Servers ist nicht Pflicht!`);
+    .setFooter(
+      `Wenn du Möchtest, Kannst du Gerne auch einen Unserem Partner Server Beitreten. Das Beitreten eines Partner Servers ist nicht Pflicht!`
+    );
   message.channel.send(Partner);
   message.delete();
 };
