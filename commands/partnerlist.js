2302b779af86a187307c2817a901960427151e52
Glitch (tkrbotiscord) <none>
2021-05-10 07:37:48 +0000

    ⏳📫 Checkpoint ./commands/partnerlist.js:6616179/224

--- commands/partnerlist.js
+++ commands/partnerlist.js
@@ -12,7 +12,7 @@ exports.run = (bot, message, args) => {
     .addField("Community \nInhaber: Miky#2546", "[Klicke Hier](https://discord.gg/QxczbuRJ87)\n**---------------**")
     .addField("Kuschelecke \nInhaber: Einfach nur ich#1844", "[Klicke Hier](https://discord.com/invite/S6uYxhvUNb)\n**---------------**")
     .addField("Team Toxic \nInhaber: Darky#4331", "[Klicke Hier](https://discord.gg/KUfR7JNV)\n**---------------**")
-    .addField("ArrowGaming \nInhaber: unknown", "[Klicke Hier](https://discord.gg/bd7Sa7rRG9)\n**---------------**")
+    .addField("ArrowGaming \nInhaber: ₳ⱤⱤØ₩₲₳₥ł₦₲#3389", "[Klicke Hier](https://discord.gg/bd7Sa7rRG9)\n**---------------**")
     .addField("GameGash \nInhaber: Cindai#0309", "[Klicke Hier](https://discord.gg/wPeJRU4qF4)\n**---------------**")
     .addField("★Chat & Gaming Lounge★ \nInhaber: flix#8194", "[Klicke Hier](https://discord.gg/u6m2Z4cbJM)\n**---------------**")
     .addField("Drip \nInhaber: Labinnac#0187", "[Klicke Hier](https://discord.gg/wNYBZrNDJW)\n**---------------**")
@@ -20,6 +20,7 @@ exports.run = (bot, message, args) => {
     .addField("CrazyEmpire \nInhaber: Ko.rex-hi#4904", "[Klicke Hier](https://discord.gg/DSr8HbmPGT)\n**---------------**")
     .addField("TXM \nInhaber: Moritztxm#5312", "[Klicke Hier](https://discord.gg/dHNTkmM)\n**---------------**")
     .addField("» VibeCrew Community \nInhaber: ! Akiroᵛᶤᵇᵉ#0001", "[Klicke Hier](https://discord.gg/qgFjCHXd6d)\n**---------------**")
+    .addField("Breadcord \nInhaber: MrBrotstange シ#1234", "[Klicke Hier](https://discord.gg/C7s4ugaQfw)\n**---------------**")
     .setColor("ORANGE")
     .setFooter(
       `Wenn du Möchtest, Kannst du Gerne auch einen Unserem Partner Server Beitreten. Das Beitreten eines Partner Servers ist nicht Pflicht!`
