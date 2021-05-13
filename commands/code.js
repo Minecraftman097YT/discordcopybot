f5993d7ef8bfc0b3ba702f3c0358470714064034
Glitch (tkrbotiscord) <none>
2021-05-13 13:38:17 +0000

    ☂️🚶 Checkpoint ./commands/code.js:6616179/23 ./storage/config.json:6616179/59 ./welcome.js:6616179/333

--- commands/code.js
+++ commands/code.js
@@ -4,9 +4,9 @@ exports.run = (bot, message, args) => {
 
     const Help = new Discord.RichEmbed()
       .setTitle("**Germany RP**", "------------------------")
-      .addField("**Code**","***Der Code von unseren RP Server ist: \n***")
-      .setColor("RED")
-      .setFooter(`.`);
+      .addField("**Code**","***Der Code von unseren RP Server ist**: \n**mFhNH**")
+      .setColor("GREEN")
+      .setFooter(``);
     message.channel.send(Help);
     message.delete()
 }
