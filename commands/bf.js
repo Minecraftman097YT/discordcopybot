bbdba03b4c258e0fb5e79f37c39eda4331e42652
Glitch (tkrbotiscord) <none>
2021-07-18 11:50:45 +0000

    🕵🐺 Checkpoint ./commands/bf.js:6616179/20

--- commands/bf.js
+++ commands/bf.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
 
     const Help = new Discord.RichEmbed()
-      .setTitle("**Prime Empire Bug-/Fehlermeldung**", "------------------------")
-      .setDescription("**Bitte füllt Folgendes Formular aus, um eine Bug-/Fehlermeldung zu Schreiben.")
-      .addField("**Formular:**","**Name: <dein name> \nType: <Bug oder Fehlermeldung> \nDatum + Uhrzeit: <Heutiges Datum + Uhrzeit> \nBeschreibung: <beschreibung des Bugs oder Fehlermeldung> \nBeweis: <screenshot, video etc.>**")
+      .setTitle("**⛔️ Prime Empire Bug-/Fehlermeldung ⛔️**", "------------------------")
+      .setDescription("**Bitte füllt Folgendes Formular aus, um eine Bug-/Fehlermeldung zu Schreiben.**")
+      .addField("**Formular:**","**> Name: <dein name> \n> Type: <Bug oder Fehlermeldung> \n> Datum + Uhrzeit: <Heutiges Datum + Uhrzeit> \n> Beschreibung: <beschreibung des Bugs oder Fehlermeldung> \n> Beweis: <screenshot, video etc.>**")
       .setColor("ORANGE")
       .setFooter(``);
     message.channel.send(Help);
