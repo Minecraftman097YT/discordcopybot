9013e4842592cf18641c4c40f67f47f901730176
Glitch (tkrbotiscord) <none>
2021-07-29 16:28:46 +0000

    👦🍲 Checkpoint ./commands/bewerbung.js:6616179/24

--- commands/bewerbung.js
+++ commands/bewerbung.js
@@ -5,7 +5,7 @@ exports.run = (bot, message, args) => {
     const Help = new Discord.RichEmbed()
       .setTitle("**:man_in_tuxedo: Prime Empire Bewerbung :woman_in_tuxedo:**", "------------------------")
       .setDescription("**Bitte füllt Folgendes Formular aus, um eine Bewerbung zu Schreiben.**")
-      .addField("**Formular:**","**> Name: <dein name> \n> Alter: <Alter> \n> Warum auf den Server: <beschreibung hier> \n> Erfahrung: <erfahrung hier>**")
+      .addField("**Formular:**","> **Name: <dein name>** \n> **Alter: <Alter>** \n> **Warum auf den Server: <beschreibung hier>** \n> **Erfahrung: <erfahrung hier>**")
       .setColor("ORANGE")
       .setFooter(``);
     message.channel.send(Help);
