d37ded39374a161e8685802fbe2115d78e8d3302
Glitch (tkrbotiscord) <none>
2021-07-31 11:24:57 +0000

    🎠🏄 Checkpoint ./commands/shop.js:6616179/446 ./commands/ddos.js:6616179/110 ./index.js:6616179/10

--- commands/shop.js
+++ commands/shop.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
 
     const Help = new Discord.RichEmbed()
-      .setTitle("**:man_in_tuxedo: Prime Empire Bewerbung :woman_in_tuxedo:**", "------------------------")
-      .setDescription("**Bitte füllt Folgendes Formular aus, um eine Bewerbung zu Schreiben.**")
-      .addField("**Formular:**","> **Name: <dein name>** \n> **Alter: <Alter>** \n> **Warum auf den Server: <beschreibung hier>** \n> **Erfahrung: <erfahrung hier>**")
+      .setTitle("**PixelBlock.EU Shop**", "------------------------")
+      .setDescription("**Hier wirst du auf unseren Shop weitergeleitet!**")
+      .addField("**Link:**","``https://shop.pixelblock.eu/`` oder **[Klicke Hier](https://shop.pixelblock.eu/)**")
       .setColor("ORANGE")
       .setFooter(``);
     message.channel.send(Help);
