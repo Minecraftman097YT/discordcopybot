7971faebce0a5820ba4092f792ebd1b95b65bcc9
Glitch (tkrbotiscord) <none>
2021-07-31 10:01:12 +0000

    ⛪️☺️ Checkpoint ./commands/team.js:6616179/1391

---
+++ commands/team.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+exports.run = (bot, message, args) => {
+
+    const Help = new Discord.RichEmbed()
+      .setTitle("**PixelBlock.EU Teamliste**", "------------------------")
+      .setDescription("**Sehe hier die Person, die im Team von PixelBlock.EU sind.**")
+      .addField("**Jetzigen Teammitglieder:**","\n**=============** \nIngame-Name ➜ **cinimini2006** \nSpitzname ➜ Fynn \nRang ➜ Owner \nAufgaben ➜ Serverleitung, Teamleitung \nAnsprechpartner für ➜ Serverteam \n**════════════════════════════════════** \nIngame-Name ➜ **GameToo** \nSpitzname ➜ Gamtu \nRang ➜ Manager \nAufgaben ➜ Organisation \nAnsprechpartner für ➜ Verwaltung")
+      .setColor("ORANGE")
+      .setFooter(``);
+    message.channel.send(Help);
+    message.delete()
+}
