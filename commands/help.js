cad5abdac5689323fd49d2b41a8312324b14ddc3
Glitch (tkrbotiscord) <none>
2021-07-31 12:11:48 +0000

    🌶🎳 Checkpoint ./commands/help.js:6616179/131 ./commands/rip.js:6616179/576

--- commands/help.js
+++ commands/help.js
@@ -6,8 +6,8 @@ exports.run = (bot, message, args) => {
   const Help = new Discord.RichEmbed()
     .setTitle("**PixelBlock.EU Bot Commands:**", "------------------------")
     .setDescription("**Alle Commands wurden in Kategorien aufgeteilt.**")
-    .addField("**Admin Commands**", "**!clear <1-100>** - Lösche Beliebig nachrichten aus dem Chat.\n**!joinvc <name>** - Lass den Bot in einen Sprachkanal gehen.\n**!poll** - Mach Umfragen mit dem Bot.\n")
-    .addField("**User Commands**", "**!help** - Lass dir diese Nachricht anschauen.\n**!ping** - Sehe den Bot-Ping.\n**!team** Sehe die Teamliste.\n**witz** - Der Bot Erzählt dir ein Witz.\n**!8ball <frage>** - Frage dem Bot eine Frage, und der Bot Antwortet mit was Vollkommend Unerwartetem.\n**!ddos <@user>** - Pranke deine Freunde indem du diesen Befehl machst.\n**!botinfo** - Sehe eine Kleine Info über dem Bot.\n**rip <@user> <nachricht>** - Lass einen Freund R.I.P Werden.")
+    .addField("**Admin Commands**", "**/clear <1-100>** - Lösche Beliebig nachrichten aus dem Chat.\n**/joinvc <name>** - Lass den Bot in einen Sprachkanal gehen.\n**/poll** - Mach Umfragen mit dem Bot.\n**/reload** - Starte den bot neu.")
+    .addField("**User Commands**", "**/help** - Lass dir diese Nachricht anschauen.\n**/ping** - Sehe den Bot-Ping.\n**/team** Sehe die Teamliste.\n**/witz** - Der Bot Erzählt dir ein Witz.\n**/8ball <frage>** - Frage dem Bot eine Frage, und der Bot Antwortet mit was Vollkommend Unerwartetem.\n**/ddos <@user>** - Pranke deine Freunde indem du diesen Befehl machst.\n**/botinfo** - Sehe eine Kleine Info über dem Bot.\n**/rip <@user> <nachricht>** - Lass einen Freund R.I.P Werden.\n**/shop** - Bekomme einen Link, der zu unserem Shop führt.")
     .setColor("FF8000")
     .setFooter(`Angefragt von: ${message.author.tag}`);
   message.channel.send(Help);
--- commands/rip.js
+++ commands/rip.js
@@ -5,16 +5,13 @@ require('moment-duration-format');
 exports.run = (bot, message, args) => {
   
   let member = message.mentions.members.first() || message.member;
-  let member1 = message.mentions.users.first() || message.author;
   let grund2 = args.slice(1).join(" ");
+  let grund1 = args[0].join(" ");
   
   if (!grund2) {
    let info = new Discord.RichEmbed()
    .setTitle(":ambulance: RIP :ambulance:")
-   .addField("User", member, true)
    .addField("Letzte Sätze", ":smiling_imp: Muhahahaha :smiling_imp:")
-   .addField("Datum" , `${moment.utc(member.now).locale('de').format("dddd, Do MMMM YYYY, HH:mm:ss")}`, true)
-   .setThumbnail(member1.avatarURL)
    .setColor("RED")
    .setTimestamp()
    message.channel.send(info)
@@ -24,15 +21,11 @@ exports.run = (bot, message, args) => {
     
        let info = new Discord.RichEmbed()
    .setTitle(":ambulance: RIP :ambulance:")
-   .addField("User", member, true)
+   .addField("Name", grund1)
    .addField("Letzte Sätze", grund2)
-   .addField("Datum" , `${moment.utc(member.now).locale('de').format("dddd, Do MMMM YYYY, HH:mm:ss")}`, true)
    .setColor("DARKBLUE")
    .setTimestamp()
-   .setThumbnail(member1.avatarURL)
    message.channel.send(info)
    message.delete()
   }
-
-   
    }
