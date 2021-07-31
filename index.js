e9815276b2a8abd513a350e0b6689e252560dcbd
Glitch (tkrbotiscord) <none>
2021-07-31 11:48:46 +0000

    🎻🎂 Checkpoint ./index.js:6616179/5052

--- index.js
+++ index.js
@@ -13,23 +13,6 @@ const fetch = require('node-fetch')
 const fs = require("fs");
 const db = require("quick.db") //using quick.db package
 
-bot.on("guildMemberAdd", (member) => { //usage of welcome event
-  let chx = db.get(`welchannel_${member.guild.id}`); //defining var
-  
-  if(chx === null) { //check if var have value or not
-    return;
-  }
-
-  let wembed = new Discord.RichEmbed() //define embed
-  .setAuthor(member.user.username, member.user.avatarURL())
-  .setColor("#ff2050")
-  .setThumbnail(member.user.avatarURL())
-  .setDescription(`We are very happy to have you in our server`);
-  
-  bot.channels.cache.get(chx).send(wembed) //get channel and send embed
-})
-
-
 const prefix = config.prefix;
 
 bot.on("message", message => {
@@ -52,21 +35,6 @@ bot.on("message", message => {
     console.log(e.stack);
   }
 });
-
-
-function doMagic8BallVoodoo() {
-  var rand = [
-    "Yes",
-    "No",
-    "Why are you even trying?",
-    "What do you think? NO",
-    "Maybe",
-    "Never",
-    "Yep"
-  ];
-
-  return rand[Math.floor(Math.random() * rand.length)];
-}
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username}, is online and is in ${bot.guilds.size}`);
@@ -104,19 +72,37 @@ bot.on('message', message => {
 
 bot.on('message', message => {
   if (message.content === 'Gut') {
-    message.channel.send('Freut mich');
+    message.channel.send('Freut mich, dass es dir gut geht.');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'gut') {
+    message.channel.send('Freut mich, dass es dir gut geht.');
   }
 });
 
 bot.on('message', message => {
   if (message.content === 'Schlecht') {
-    message.channel.send('Ok. Ich Hoffe, Dir geht es bald Besser.');
+    message.channel.send('Ok. Ich hoffe, dir geht es bald besser.');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'schlecht') {
+    message.channel.send('Ok. Ich hoffe, dir geht es bald besser.');
   }
 });
 
 bot.on('message', message => {
   if (message.content === 'lol') {
-    message.channel.send('Was? Findest du es Etwa Lustig? Komisch...');
+    message.channel.send('Was? Findest du das etwa lustig? Komisch...');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'Lol') {
+    message.channel.send('Was? Findest du das etwa lustig? Komisch...');
   }
 });
 
@@ -127,144 +113,25 @@ bot.on('message', message => {
 })
 
 bot.on('message', message => {
-  if (message.content === 'Elmar') {
-    message.channel.send('<@793165733185978448>')
+  if (message.content === 'nice') {
+    message.channel.send('Cool! Freut mich!')
   }
 })
 
-bot.on('message', message => {
-  if (message.content === 'welcome') {
-    message.channel.send('<a:info:827806069404205076> **Willkommen Neuling!** <a:info:827806069404205076>\n<a:info:827806069404205076> **Schau mal in <#810900110053081121> Vorbei!** <a:info:827806069404205076>');
-  }
-});
+
 
 bot.on('message', message => {
-  if (message.content === 'wilkommen') {
+  if (message.content === 'willkommen') {
     message.channel.send('╔══════ ≪≫°✺°≪ ≫ ══════╗\n♥♡♥♡♥**Willkommen**♥♡♥♡♥\n╚══════ ≪≫°✺°≪ ≫ ══════╝');
   }
 });
 
 bot.on('message', message => {
-  if (message.content === 'A') {
-    message.reply('B')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'C') {
-    message.reply('D')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'E') {
-    message.reply('F')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'G') {
-    message.reply('H')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'I') {
-    message.reply('J')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'K') {
-    message.reply('L')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'M') {
-    message.reply('N')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'O') {
-    message.reply('P')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'Q') {
-    message.reply('R')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'S') {
-    message.reply('T')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'U') {
-    message.reply('V')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'W') {
-    message.reply('X')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'Y') {
-    message.reply('Z')
-  }
-})
-
-bot.on('message', message => {
-  if (message.content === 'Z') {
-    message.reply('Wir sind schon beim Ende .-.')
-  }
-})
-
-
-//Welcome-Bye
-
-//Welcome & goodbye messages\\
-bot.on('guildMemberAdd', member => {
-    member.roles.add(member.guild.roles.cache.find(i => i.name === ''))
-
-    const welcomeEmbed = new Discord.MessageEmbed()
-
-    welcomeEmbed.setColor('#5cf000')
-    welcomeEmbed.setTitle('*Willkommen!*' + member.user.username + '** Du bist nun Mitglied Nummer: **' + member.guild.memberCount + '**')
-    welcomeEmbed.setImage('https://cdn.mos.cms.futurecdn.net/93GAa4wm3z4HbenzLbxWeQ-650-80.jpg.webp')
-
-    member.guild.channels.cache.find(i => i.name === 'hauptchat').send(welcomeEmbed)
-})
-
-bot.on('guildMemberRemove', member => {
-    const goodbyeEmbed = new Discord.MessageEmbed()
-
-    goodbyeEmbed.setColor('#f00000')
-    goodbyeEmbed.setTitle('**' + member.user.username + '** Hat uns Verlassen! \n Wir Sind nurnoch**' + member.guild.memberCount + '** Mitglieder **')
-    goodbyeEmbed.setImage('https://gamewith-en.akamaized.net/article/thumbnail/rectangle/22183.png')
-
-    member.guild.channels.cache.find(i => i.name === 'hauptchat').send(goodbyeEmbed)
-})
-//Welcome & goodbye messages end\\
-
-//Irgendwas
-bot.on("message", message => {
-   if (message.content.toLowerCase() === 'TMSf8ball') {
-    let sEmbed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setTitle("8ball")
-  .addDescriptition('Your anwser is:' + doMagic8BallVoodoo())
-    message.channel.send(sEmbed)
+  if (message.content === 'Willkommen') {
+    message.channel.send('╔══════ ≪≫°✺°≪ ≫ ══════╗\n♥♡♥♡♥**Willkommen**♥♡♥♡♥\n╚══════ ≪≫°✺°≪ ≫ ══════╝');
   }
 });
+
 setInterval(async () => {
   await fetch("https://tkrbotiscord.glitch.me").then(console.log("pinged"));
 }, 22000);
